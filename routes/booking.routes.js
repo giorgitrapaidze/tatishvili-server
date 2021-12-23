9f8e8971a51e1773e65d14415c09654187f607ad
giorgitrapaidze <gio.trapaidze@gmail.com>
2021-12-23 15:57:47 +0400

    added update and delete

--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -20,6 +20,23 @@ router.post("/api/booking", (req, res, next) => {
     .catch((err) => res.json(err));
 });
 
+router.delete("/api/booking/:bookingId", (req, res, next) => {
+  const { bookingId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(bookingId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Booking.findByIdAndRemove(bookingId)
+    .then(() =>
+      res.json({
+        message: `Booking with ${projectId} is removed successfully.`,
+      })
+    )
+    .catch((error) => res.json(error));
+});
+
 router.get("/api/bookings/:userId", async (req, res, next) => {
   const { userId } = req.params;
   let username = mongoose.Types.ObjectId(userId);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
 const { isAuthenticated } = require("./../middleware/jwt.middleware");
-
+const jwt = require("jsonwebtoken");
 
 // GET /api/users/current  - Get current user info
-router.get('/api/users/current', isAuthenticated, async (req, res, next) => {
+router.get("/api/users/current", isAuthenticated, async (req, res, next) => {
   try {
     // If the user is authenticated we can access the JWT payload via req.payload
     // req.payload holds the user info that was encoded in JWT during login.
-  
+
     const currentUser = req.payload;
     const user = await User.findById(currentUser._id);
 
@@ -17,14 +17,14 @@ router.get('/api/users/current', isAuthenticated, async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-})
+});
 
 // PUT /api/users/current  - Update the current user
-router.put('/api/users/current', isAuthenticated, async (req, res, next) => {
+router.put("/api/users/current", isAuthenticated, async (req, res, next) => {
   try {
     // If the user is authenticated we can access the JWT payload via req.payload
     // req.payload holds the user info that was encoded in JWT during login.
-  
+
     const currentUser = req.payload;
     const { email, name } = req.body;
 
@@ -34,11 +34,22 @@ router.put('/api/users/current', isAuthenticated, async (req, res, next) => {
       { new: true }
     );
 
-    res.status(200).json(updatedUser);
+    const payload = {
+      _id: updatedUser._id,
+      email: updatedUser.email,
+      name: updatedUser.name,
+      role: updatedUser.role, // 'admin' or 'user'
+      image: updatedUser.image,
+    };
+    const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "12h",
+    });
+
+    res.status(200).json({ authToken: authToken });
   } catch (error) {
     next(error);
   }
-})
-
+});
 
 module.exports = router;
